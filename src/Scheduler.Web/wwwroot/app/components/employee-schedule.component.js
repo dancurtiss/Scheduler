e546603a2d8e5f0ab8dd152da32f7b00afd8641a
Dan Curtiss <dancurtiss@gmail.com>
2016-10-30 13:19:49 -0500

    Get dragula working with employees and shifts

--- src/Scheduler.Web/wwwroot/app/components/employee-schedule.component.js
+++ src/Scheduler.Web/wwwroot/app/components/employee-schedule.component.js
@@ -10,16 +10,18 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var router_1 = require('@angular/router');
+var ng2_dragula_1 = require('ng2-dragula/ng2-dragula');
 var employee_schedule_service_1 = require('../services/employee-schedule.service');
 var moment = require('moment');
 /**
  * Need to figure out the UI of picking a schedule for a day... need schema here..
  */
 var EmployeeScheduleComponent = (function () {
-    function EmployeeScheduleComponent(employeeScheduleService, router, route) {
+    function EmployeeScheduleComponent(employeeScheduleService, router, route, dragulaService) {
         this.employeeScheduleService = employeeScheduleService;
         this.router = router;
         this.route = route;
+        this.dragulaService = dragulaService;
         this.scheduleDate = new Date();
     }
     EmployeeScheduleComponent.prototype.getSchedule = function () {
@@ -29,6 +31,14 @@ var EmployeeScheduleComponent = (function () {
             _this.availableEmployees = model.employees;
             _this.availableShifts = model.shifts;
             _this.employeeShifts = model.employeeShifts;
+            _this.setupShiftBags();
+        });
+    };
+    EmployeeScheduleComponent.prototype.setupShiftBags = function () {
+        var _this = this;
+        this.shiftBags = {};
+        this.availableShifts.forEach(function (s) {
+            _this.shiftBags[s.shiftId] = [];
         });
     };
     EmployeeScheduleComponent.prototype.ngOnInit = function () {
@@ -37,8 +47,78 @@ var EmployeeScheduleComponent = (function () {
             var id = +params['id'];
             _this.organizationId = id;
         });
+        this.dragulaSetup();
         this.scheduleDate = new Date();
         this.getSchedule();
+    };
+    EmployeeScheduleComponent.prototype.remove = function (employeeId, shiftId) {
+        var removes = this.shiftBags[shiftId].filter(function (es) {
+            return es.employeeId == employeeId;
+        });
+        var index = this.shiftBags[shiftId].indexOf(removes[0]);
+        if (index > -1) {
+            this.shiftBags[shiftId].splice(index, 1);
+        }
+    };
+    EmployeeScheduleComponent.prototype.dragulaSetup = function () {
+        var _this = this;
+        this.dragulaService.setOptions('schedule-bag', {
+            removeOnSpill: true,
+            copy: true,
+            moves: function (el, source, handle, sibling) {
+                // only move favorite items, not the icon element
+                return el.className.toLowerCase() === 'employee-item';
+            },
+            accepts: function (el, target, source, sibling) {
+                return !el.contains(target); // elements can not be dropped within themselves
+            },
+            invalid: function (el, handle) {
+                return false; // don't prevent any drags from initiating by default
+            }
+        });
+        this.dragulaService.dropModel.subscribe(function (value) {
+            console.log('ouch', value);
+            _this.onDropModel(value.slice(1));
+        });
+        this.dragulaService.removeModel.subscribe(function (value) {
+            _this.onRemoveModel(value.slice(1));
+        });
+        this.dragulaService.drag.subscribe(function (value) {
+            _this.onDrag(value.slice(1));
+        });
+        //this.dragulaService.drop.subscribe((value) => {
+        //    this.onDrop(value.slice(1));
+        //});
+        this.dragulaService.over.subscribe(function (value) {
+            _this.onOver(value.slice(1));
+        });
+        this.dragulaService.out.subscribe(function (value) {
+            _this.onOut(value.slice(1));
+        });
+    };
+    EmployeeScheduleComponent.prototype.onDropModel = function (args) {
+        var el = args[0], target = args[1], source = args[2];
+        console.log("dropmodel: " + args);
+    };
+    EmployeeScheduleComponent.prototype.onRemoveModel = function (args) {
+        var el = args[0], source = args[1];
+        console.log("removemodel: " + args);
+    };
+    EmployeeScheduleComponent.prototype.onDrag = function (args) {
+        var e = args[0], el = args[1];
+        //console.log(`drag: ${args}`);
+    };
+    EmployeeScheduleComponent.prototype.onDrop = function (args) {
+        var e = args[0], el = args[1];
+        //console.log(`drop: ${args}`);
+    };
+    EmployeeScheduleComponent.prototype.onOver = function (args) {
+        var e = args[0], el = args[1], container = args[2];
+        //console.log(`over: ${args}`);
+    };
+    EmployeeScheduleComponent.prototype.onOut = function (args) {
+        var e = args[0], el = args[1], container = args[2];
+        //console.log(`out: ${args}`);
     };
     //onSaveEmployee(employeeId: number, name: string, contactName: string, contactPhone: string, message: string): void {
     //    if (this.selectedEmployee.employeeId) {
@@ -66,7 +146,7 @@ var EmployeeScheduleComponent = (function () {
             templateUrl: 'employee-schedule.component.html',
             styleUrls: ['employee-schedule.component.css']
         }), 
-        __metadata('design:paramtypes', [employee_schedule_service_1.EmployeeScheduleService, router_1.Router, router_1.ActivatedRoute])
+        __metadata('design:paramtypes', [employee_schedule_service_1.EmployeeScheduleService, router_1.Router, router_1.ActivatedRoute, ng2_dragula_1.DragulaService])
     ], EmployeeScheduleComponent);
     return EmployeeScheduleComponent;
 }());
--- src/Scheduler.Web/wwwroot/app/components/employee-schedule.component.ts
+++ src/Scheduler.Web/wwwroot/app/components/employee-schedule.component.ts
@@ -1,6 +1,6 @@
 ﻿import { Component, OnInit }        from '@angular/core';
 import { Router, ActivatedRoute, Params }      from '@angular/router';
-import { DragulaModule } from 'ng2-dragula/ng2-dragula'
+import { DragulaService } from 'ng2-dragula/ng2-dragula';
 
 import { EmployeeSchedule, EmployeeDisplay, EmployeeShift, ShiftDisplay }             from '../models/employee-schedule';
 import { Position }             from '../models/schedule';
@@ -21,12 +21,12 @@ import * as moment from 'moment'
 export class EmployeeScheduleComponent implements OnInit {
 
     organizationId: number;
-    availableEmployees: EmployeeDisplay[];
-    selectedEmployee: EmployeeDisplay;
 
+    availableEmployees: EmployeeDisplay[];
+
+    shiftBags: any;
 
     availableShifts: ShiftDisplay[];
-
     employeeShifts: EmployeeShift[];
 
     scheduleDate: Date = new Date();
@@ -34,7 +34,8 @@ export class EmployeeScheduleComponent implements OnInit {
     constructor(
         private employeeScheduleService: EmployeeScheduleService,
         private router: Router,
-        private route: ActivatedRoute
+        private route: ActivatedRoute,
+        private dragulaService: DragulaService
     ) { }
 
     getSchedule(): void {
@@ -43,6 +44,15 @@ export class EmployeeScheduleComponent implements OnInit {
             this.availableEmployees = model.employees;
             this.availableShifts = model.shifts;
             this.employeeShifts = model.employeeShifts;
+
+            this.setupShiftBags();
+        });
+    }
+
+    setupShiftBags() {
+        this.shiftBags = {};
+        this.availableShifts.forEach((s) => {
+            this.shiftBags[s.shiftId] = [];
         });
     }
 
@@ -52,10 +62,90 @@ export class EmployeeScheduleComponent implements OnInit {
             this.organizationId = id;
         });
 
+        this.dragulaSetup();
         this.scheduleDate = new Date();
         this.getSchedule();
+
     }
 
+    remove(employeeId: number, shiftId: number) {
+        var removes = this.shiftBags[shiftId].filter((es) => {
+            return es.employeeId == employeeId;
+        });
+
+        var index = this.shiftBags[shiftId].indexOf(removes[0]);
+
+        if (index > -1) {
+            this.shiftBags[shiftId].splice(index, 1);
+        }
+    }
+
+    dragulaSetup() {
+        this.dragulaService.setOptions('schedule-bag', {
+            removeOnSpill: true,
+            copy: true,
+            moves: function (el, source, handle, sibling) {
+                // only move favorite items, not the icon element
+                return el.className.toLowerCase() === 'employee-item';
+            },
+            accepts: function (el, target, source, sibling) {
+                return !el.contains(target); // elements can not be dropped within themselves
+            },
+            invalid: function (el, handle) {
+                return false; // don't prevent any drags from initiating by default
+            }
+        });
+
+        this.dragulaService.dropModel.subscribe((value) => {
+            console.log('ouch', value);
+            this.onDropModel(value.slice(1));
+        });
+        this.dragulaService.removeModel.subscribe((value) => {
+            this.onRemoveModel(value.slice(1));
+        });
+        this.dragulaService.drag.subscribe((value) => {
+            this.onDrag(value.slice(1));
+        });
+        //this.dragulaService.drop.subscribe((value) => {
+        //    this.onDrop(value.slice(1));
+        //});
+        this.dragulaService.over.subscribe((value) => {
+            this.onOver(value.slice(1));
+        });
+        this.dragulaService.out.subscribe((value) => {
+            this.onOut(value.slice(1));
+        });
+    }
+
+    private onDropModel(args) {
+        let [el, target, source] = args;
+        console.log(`dropmodel: ${args}`);
+    }
+
+    private onRemoveModel(args) {
+        let [el, source] = args;
+        console.log(`removemodel: ${args}`);
+    }
+
+    private onDrag(args) {
+        let [e, el] = args;
+        //console.log(`drag: ${args}`);
+    }
+
+    private onDrop(args) {
+        let [e, el] = args;
+        //console.log(`drop: ${args}`);
+    }
+
+    private onOver(args) {
+        let [e, el, container] = args;
+        //console.log(`over: ${args}`);
+    }
+
+    private onOut(args) {
+        let [e, el, container] = args;
+        //console.log(`out: ${args}`);
+    }
 
     //onSaveEmployee(employeeId: number, name: string, contactName: string, contactPhone: string, message: string): void {
     //    if (this.selectedEmployee.employeeId) {
